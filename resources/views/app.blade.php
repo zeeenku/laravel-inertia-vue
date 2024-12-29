d9eeaf9c412983c9795cf0866e4974e2c4cb0a0b
zeeenku <enajjachi.zakariaa@gmail.com>
2024-12-29 01:14:20 +0100

    redirect as spa & passe data to root template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,8 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <title>{{$page["props"]["title"]}}</title>
+    <meta name="description" content="{{ $description }}">
+
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     @inertiaHead
 </head>
--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,18 @@ Route::get('/', function () {
 
     return Inertia::render('Welcome', [
         "title"=>"Welcome to Laravel with Inertia.js and Vue.jsss"
-    ]);
+        //only passed to root
+    ])->withViewData(['description' => "description hhh"]);
+    ;
+})->name("home");
+
+
+
+
+Route::get('/red-home', function () {  
+
+    sleep(2);
+    return to_route('home'); // redirects to home as spa
+
 });
+
